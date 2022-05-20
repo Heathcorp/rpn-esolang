9d5f9877b2b20d1c939c29620d6a34e66641d074
Heathcorp <heath@manningf.com>
2022-05-20 11:49:47 +1000

    Implement looping

--- rpnesolang.py
+++ rpnesolang.py
@@ -6,108 +6,128 @@ import re
 
 from matplotlib import offsetbox
 
-def handle_instruction(stack: list, line: str):
-	
-	if re.search('^(#.*)?$', line):
-		# comment or empty line, ignore
-		pass
+# main stack memory
+stack = []
+# program memory
+pc = 0
+lines = []
+# loop stack
+loops = []
+skipLoop = False
+loopToSkip = -1
 
-	elif re.search('^-?[0-9]+$', line):
+def handle_instruction(line: str):
+	global skipLoop
+	global pc
+	global loopToSkip
+	if re.search('^(#.*)?$', line) and not skipLoop:
+		# comment or empty line, ignore
+		return False
+
+	elif re.search('^-?[0-9]+$', line) and not skipLoop:
 		# integer, push to stack
 		stack.append(int(line))
 
-	elif re.search('^-?[0-9]+\.?[0-9]+$', line):
+	elif re.search('^-?[0-9]+\.?[0-9]+$', line) and not skipLoop:
 		# floating point number, push to stack
 		stack.append(float(line))
 
-	elif re.search('^\'.*\'$', line):
+	elif re.search('^\'.*\'$', line) and not skipLoop:
 		# string, parse and push to stack
 		stack.append(str(line[1:len(line) - 2]))
 
-	elif re.search('^\+$', line):
+	elif re.search('^\+$', line) and not skipLoop:
 		# addition
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 + value2)
 
-	elif re.search('^\-$', line):
+	elif re.search('^\-$', line) and not skipLoop:
 		# subtraction
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 - value2)
 
-	elif re.search('^\*$', line):
+	elif re.search('^\*$', line) and not skipLoop:
 		# multiplication
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 * value2)
 
-	elif re.search('^\/$', line):
+	elif re.search('^\/$', line) and not skipLoop:
 		# division
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 / value2)
 
-	elif re.search('^=$', line):
+	elif re.search('^=$', line) and not skipLoop:
 		# equality
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 == value2)
 
-	elif re.search('^>$', line):
+	elif re.search('^>$', line) and not skipLoop:
 		# more than
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 > value2)
 
-	elif re.search('^<$', line):
+	elif re.search('^<$', line) and not skipLoop:
 		# less than
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 < value2)
 
-	elif re.search('^>=$', line):
+	elif re.search('^>=$', line) and not skipLoop:
 		# more than or equal
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 >= value2)
 
-	elif re.search('^<=$', line):
+	elif re.search('^<=$', line) and not skipLoop:
 		# less than or equal
-		value1 = stack[len(stack) - 2]
-		value2 = stack[len(stack) - 1]
-		stack.pop()
-		stack.pop()
+		value2 = stack.pop()
+		value1 = stack.pop()
 		stack.append(value1 <= value2)
 
 	# everything is highly experimental, but the following instructions especially
-	elif re.search('^\[[0-9]+\]$', line):
+	elif re.search('^\[[0-9]+\]$', line) and not skipLoop:
 		# copy nth stack element (from the top down)
 		offset = int(line[1:len(line) - 2])
 		value = stack[len(stack) - 1 - offset]
 		stack.append(value)
 
-	elif re.search('^<<$', line):
+	elif re.search('^<<$', line) and not skipLoop:
 		# print and pop the element at the top of the stack
-		value = stack[len(stack) - 1]
-		stack.pop()
+		value = stack.pop()
 		print(value)
+
+	elif re.search('^{$', line):
+		# pops top of stack, if it was 0 then skip to the closing brace
+		loops.append(pc)
+		if not skipLoop:
+			value = stack.pop()
+			if value == 0:
+				skipLoop = True
+				loopToSkip = pc
+			
+				
+
+	elif re.search('^}$', line):
+		# pops top of stack, if it was not 0 then go back to the opening brace
+		loopStart = loops.pop()
+		if not skipLoop:
+			value = stack.pop()
+			if value != 0:
+				pc = loopStart
+				loops.append(loopStart)
+
+		elif loopToSkip == loopStart:
+			# reached the end of the skip
+			loopToSkip = -1
+			skipLoop = False
+
+	return True
 
 
 if __name__ == '__main__':
@@ -117,14 +137,13 @@ if __name__ == '__main__':
 
 	filename = sys.argv[1]
 
-	# main memory stack
-	stack = []
-
 	with open(filename, 'r') as file:
 		line = file.readline()
 
 		while line:
-			handle_instruction(stack, line)
-			print(stack)
-
+			lines.append(line)
 			line = file.readline()
+
+		while pc < len(lines):
+			if handle_instruction(lines[pc]): pass #print(stack, loops, skipLoop, pc)
+			pc += 1
